5a59baf029d6686c97a8d0c2debc04e37146eb99
Oleg Primolennyy <element.onex@gmail.com>
2018-04-08 20:34:41 +0300

    добавил отправку ботом сообщения с текстом полученного письма назад отправителю

--- src/main/java/botapp/bot/TelegramBot.java
+++ src/main/java/botapp/bot/TelegramBot.java
@@ -1,13 +1,29 @@
 package botapp.bot;
 
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
 
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage()
+                    .setChatId(chatId)
+                    .setText("Your message was: " + messageText);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
